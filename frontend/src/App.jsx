f97d1bcd9c17f195954f196214f7fc1de5bf88a7
Hthoni <hthoni@hotmail.com>
2026-08-27 09:12:44 -0300

    Add protected routes for various components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import NavBar from "./components/NavBar";
+import RotaProtegida from "./components/RotaProtegida";
 
 import Busca from "./pages/Busca";
 import Resultados from "./pages/Resultados";
@@ -12,6 +13,7 @@ import ConversasRecebidas from "./pages/ConversasRecebidas";
 import MinhaContaCliente from "./pages/MinhaContaCliente";
 import MinhaContaEmpresa from "./pages/MinhaContaEmpresa";
 import AdminPainel from "./pages/AdminPainel";
+import NaoEncontrado from "./pages/NaoEncontrado";
 
 export default function App() {
   return (
@@ -24,13 +26,28 @@ export default function App() {
           <Route path="/resultados" element={<Resultados />} />
           <Route path="/entrar" element={<Entrar tipoInicial="cliente" />} />
           <Route path="/empresa/entrar" element={<Entrar tipoInicial="empresa" />} />
-          <Route path="/conversas" element={<MinhasConversas />} />
-          <Route path="/conversas/:id" element={<ConversaDetalhe />} />
-          <Route path="/estoque" element={<EstoqueEmpresa />} />
-          <Route path="/conversas-recebidas" element={<ConversasRecebidas />} />
-          <Route path="/minha-conta" element={<MinhaContaCliente />} />
-          <Route path="/empresa/minha-conta" element={<MinhaContaEmpresa />} />
+
+          <Route path="/conversas" element={
+            <RotaProtegida tipo="cliente"><MinhasConversas /></RotaProtegida>
+          } />
+          <Route path="/conversas/:id" element={
+            <RotaProtegida tipo="qualquer"><ConversaDetalhe /></RotaProtegida>
+          } />
+          <Route path="/estoque" element={
+            <RotaProtegida tipo="empresa"><EstoqueEmpresa /></RotaProtegida>
+          } />
+          <Route path="/conversas-recebidas" element={
+            <RotaProtegida tipo="empresa"><ConversasRecebidas /></RotaProtegida>
+          } />
+          <Route path="/minha-conta" element={
+            <RotaProtegida tipo="cliente"><MinhaContaCliente /></RotaProtegida>
+          } />
+          <Route path="/empresa/minha-conta" element={
+            <RotaProtegida tipo="empresa"><MinhaContaEmpresa /></RotaProtegida>
+          } />
+
           <Route path="/admin" element={<AdminPainel />} />
+          <Route path="*" element={<NaoEncontrado />} />
         </Routes>
       </HashRouter>
     </AuthProvider>
