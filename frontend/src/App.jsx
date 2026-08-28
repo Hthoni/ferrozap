d3f8a7650450aae73ff495e7c7ea68dc94f0c0f5
Hthoni <hthoni@hotmail.com>
2026-08-28 06:22:56 -0300

    Add Footer component and restructure Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 import RotaProtegida from "./components/RotaProtegida";
 
 import Busca from "./pages/Busca";
@@ -20,35 +21,38 @@ export default function App() {
     <AuthProvider>
       <HashRouter>
         <NavBar />
-        <Routes>
-          <Route path="/" element={<Busca />} />
-          <Route path="/buscar" element={<Busca />} />
-          <Route path="/resultados" element={<Resultados />} />
-          <Route path="/entrar" element={<Entrar tipoInicial="cliente" />} />
-          <Route path="/empresa/entrar" element={<Entrar tipoInicial="empresa" />} />
+        <main>
+          <Routes>
+            <Route path="/" element={<Busca />} />
+            <Route path="/buscar" element={<Busca />} />
+            <Route path="/resultados" element={<Resultados />} />
+            <Route path="/entrar" element={<Entrar tipoInicial="cliente" />} />
+            <Route path="/empresa/entrar" element={<Entrar tipoInicial="empresa" />} />
 
-          <Route path="/conversas" element={
-            <RotaProtegida tipo="cliente"><MinhasConversas /></RotaProtegida>
-          } />
-          <Route path="/conversas/:id" element={
-            <RotaProtegida tipo="qualquer"><ConversaDetalhe /></RotaProtegida>
-          } />
-          <Route path="/estoque" element={
-            <RotaProtegida tipo="empresa"><EstoqueEmpresa /></RotaProtegida>
-          } />
-          <Route path="/conversas-recebidas" element={
-            <RotaProtegida tipo="empresa"><ConversasRecebidas /></RotaProtegida>
-          } />
-          <Route path="/minha-conta" element={
-            <RotaProtegida tipo="cliente"><MinhaContaCliente /></RotaProtegida>
-          } />
-          <Route path="/empresa/minha-conta" element={
-            <RotaProtegida tipo="empresa"><MinhaContaEmpresa /></RotaProtegida>
-          } />
+            <Route path="/conversas" element={
+              <RotaProtegida tipo="cliente"><MinhasConversas /></RotaProtegida>
+            } />
+            <Route path="/conversas/:id" element={
+              <RotaProtegida tipo="qualquer"><ConversaDetalhe /></RotaProtegida>
+            } />
+            <Route path="/estoque" element={
+              <RotaProtegida tipo="empresa"><EstoqueEmpresa /></RotaProtegida>
+            } />
+            <Route path="/conversas-recebidas" element={
+              <RotaProtegida tipo="empresa"><ConversasRecebidas /></RotaProtegida>
+            } />
+            <Route path="/minha-conta" element={
+              <RotaProtegida tipo="cliente"><MinhaContaCliente /></RotaProtegida>
+            } />
+            <Route path="/empresa/minha-conta" element={
+              <RotaProtegida tipo="empresa"><MinhaContaEmpresa /></RotaProtegida>
+            } />
 
-          <Route path="/admin" element={<AdminPainel />} />
-          <Route path="*" element={<NaoEncontrado />} />
-        </Routes>
+            <Route path="/admin" element={<AdminPainel />} />
+            <Route path="*" element={<NaoEncontrado />} />
+          </Routes>
+        </main>
+        <Footer />
       </HashRouter>
     </AuthProvider>
   );
