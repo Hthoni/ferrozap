1e6dc82dd6528e05f619f63dc25f9689fa553d48
Hthoni <hthoni@hotmail.com>
2026-09-02 07:12:54 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,48 +19,56 @@ import EsqueciSenha from "./pages/EsqueciSenha";
 import RedefinirSenha from "./pages/RedefinirSenha";
 import NaoEncontrado from "./pages/NaoEncontrado";
 
+function Layout() {
+  return (
+    <div className="cs-fundo-padrao">
+      <NavBar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Busca />} />
+          <Route path="/buscar" element={<Busca />} />
+          <Route path="/resultados" element={<Resultados />} />
+          <Route path="/entrar" element={<Entrar tipoInicial="cliente" />} />
+          <Route path="/empresa/entrar" element={<Entrar tipoInicial="empresa" />} />
+          <Route path="/esqueci-senha" element={<EsqueciSenha tipoInicial="usuario_final" />} />
+          <Route path="/redefinir-senha" element={<RedefinirSenha />} />
+          <Route path="/admin/entrar" element={<AdminLogin />} />
+
+          <Route path="/conversas" element={
+            <RotaProtegida tipo="cliente"><MinhasConversas /></RotaProtegida>
+          } />
+          <Route path="/conversas/:id" element={
+            <RotaProtegida tipo="qualquer"><ConversaDetalhe /></RotaProtegida>
+          } />
+          <Route path="/estoque" element={
+            <RotaProtegida tipo="empresa"><EstoqueEmpresa /></RotaProtegida>
+          } />
+          <Route path="/conversas-recebidas" element={
+            <RotaProtegida tipo="empresa"><ConversasRecebidas /></RotaProtegida>
+          } />
+          <Route path="/minha-conta" element={
+            <RotaProtegida tipo="cliente"><MinhaContaCliente /></RotaProtegida>
+          } />
+          <Route path="/empresa/minha-conta" element={
+            <RotaProtegida tipo="empresa"><MinhaContaEmpresa /></RotaProtegida>
+          } />
+
+          <Route path="/admin" element={
+            <RotaProtegida tipo="admin"><AdminPainel /></RotaProtegida>
+          } />
+          <Route path="*" element={<NaoEncontrado />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
 export default function App() {
   return (
     <AuthProvider>
       <HashRouter>
-        <NavBar />
-        <main>
-          <Routes>
-            <Route path="/" element={<Busca />} />
-            <Route path="/buscar" element={<Busca />} />
-            <Route path="/resultados" element={<Resultados />} />
-            <Route path="/entrar" element={<Entrar tipoInicial="cliente" />} />
-            <Route path="/empresa/entrar" element={<Entrar tipoInicial="empresa" />} />
-            <Route path="/esqueci-senha" element={<EsqueciSenha tipoInicial="usuario_final" />} />
-            <Route path="/redefinir-senha" element={<RedefinirSenha />} />
-            <Route path="/admin/entrar" element={<AdminLogin />} />
-
-            <Route path="/conversas" element={
-              <RotaProtegida tipo="cliente"><MinhasConversas /></RotaProtegida>
-            } />
-            <Route path="/conversas/:id" element={
-              <RotaProtegida tipo="qualquer"><ConversaDetalhe /></RotaProtegida>
-            } />
-            <Route path="/estoque" element={
-              <RotaProtegida tipo="empresa"><EstoqueEmpresa /></RotaProtegida>
-            } />
-            <Route path="/conversas-recebidas" element={
-              <RotaProtegida tipo="empresa"><ConversasRecebidas /></RotaProtegida>
-            } />
-            <Route path="/minha-conta" element={
-              <RotaProtegida tipo="cliente"><MinhaContaCliente /></RotaProtegida>
-            } />
-            <Route path="/empresa/minha-conta" element={
-              <RotaProtegida tipo="empresa"><MinhaContaEmpresa /></RotaProtegida>
-            } />
-
-            <Route path="/admin" element={
-              <RotaProtegida tipo="admin"><AdminPainel /></RotaProtegida>
-            } />
-            <Route path="*" element={<NaoEncontrado />} />
-          </Routes>
-        </main>
-        <Footer />
+        <Layout />
       </HashRouter>
     </AuthProvider>
   );
